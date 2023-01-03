c9f33e2cb75dd38753ea13b75617b9d9b0c0c165
MoAfifi0 <m.afifi00000gmail.com>
2023-01-03 01:54:28 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for word in str:
         if (ord(word) > 96 and ord(word) <= 124):
             word = chr(ord(word) - 32)
-        print("{}".format(word), end=''
+        print("{}".format(word), end='')
     print()
