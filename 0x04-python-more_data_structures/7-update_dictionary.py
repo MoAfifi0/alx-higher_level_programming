f294391539a0ab1f90e573147c9ffb6e6c9a6bc7
MoAfifi0 <m.afifi00000gmail.com>
2023-01-10 23:07:56 -0800

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+def update_dictionary(a_dictionary, key, value):
+    """
+    a function that replaces or adds key/value in a dictionary.
+    key argument will be always a string
+    value argument will be any type
+    """
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
