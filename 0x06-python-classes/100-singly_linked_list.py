48e6801c27ac6cf5d9a3c0f11907e475f3e7de6b
MoAfifi0 <m.afifi00000gmail.com>
2023-01-24 15:25:19 -0800

    100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,6 +7,7 @@ class Node:
 
     def __init__(self, data, next_node=None):
         """Initialize a new Node.
+
         Args:
             data (int): The data of the new Node.
             next_node (Node): The next node of the new Node.
